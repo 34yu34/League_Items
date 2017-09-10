0cfa0cbd90cde031f7a3c127a01a8c786ee0e830
Hyftar <simon_landry@ymail.com>
2017-09-09 23:17:26 -0400

    Add summoner class

--- main.rb
+++ main.rb
@@ -2,6 +2,18 @@ require 'net/http'
 require 'json'
 require 'irb'
 
+class Summoner
+  attr_accessor :name, :tier, :dision, :league_points
+
+  def initialize(options)
+    @name = options[:name]
+    @tier = get_tier_level(options[:tier])
+    @division = get_rank_level(options[:division])
+    @league_points = options[:lp]
+  end
+end
+
+
 KEY = "YOUR_KEY_HERE"
 
 def create_request(request)
@@ -32,4 +44,17 @@ def get_rank_level(rank)
     "IV",
     "V",
   ].index(rank.upcase) + 1
+end
+
+def get_summoner_rank(summonerId)
+  league_rank = JSON.parse(get(create_request("lol/league/v3/positions/by-summoner/#{summonerId}")))
+    .select { |x| x['queueType'] == 'RANKED_SOLO_5x5' }
+    .first
+
+  Summoner.new(
+    name: league_rank['playerOrTeamName'],
+    tier: league_rank['tier'],
+    division: league_rank['rank'],
+    lp: league_rank['leaguePoints']
+  )
 end
